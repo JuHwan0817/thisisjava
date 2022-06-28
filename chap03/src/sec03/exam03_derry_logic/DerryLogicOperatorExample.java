b0f046f309b3034aa236dd5fdca573487ae18b67
JuHwan <juhwan203@kakao.com>
2022-06-28 14:01:37 +0900

    IfElseExample.java

---
+++ chap03/src/sec03/exam03_derry_logic/DerryLogicOperatorExample.java
@@ -0,0 +1,17 @@
+package sec03.exam03_derry_logic;
+
+public class DerryLogicOperatorExample {
+
+	public static void main(String[] args) {
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+
+	}
+
+}
---
+++ chap03/src/sec03/exam04_bit_reverse/BitReverseOeratorExample.java
@@ -0,0 +1,30 @@
+package sec03.exam04_bit_reverse;
+
+public class BitReverseOeratorExample {
+
+	public static void main(String[] args) {
+		int v1 = 10;
+		int v2 = ~v1;
+		int v3 = ~v1 + 1;
+		System.out.println(toBinaryString(v1) + "(십진수: " + v1 + ")");
+		System.out.println(toBinaryString(v2) + "(십진수: " + v2 + ")");
+		System.out.println(toBinaryString(v3) + "(십진수: " + v3 + ")");
+		
+		int v4 = -10;
+		int v5 = ~v4;
+		int v6 = ~v4 + 1;
+		System.out.println(toBinaryString(v4) + "(십진수: " + v4 + ")");
+		System.out.println(toBinaryString(v5) + "(십진수: " + v5 + ")");
+		System.out.println(toBinaryString(v6) + "(십진수: " + v6 + ")");
+		
+
+	}
+	public static String tobinaryString(int value) {
+		String str = Integer.toBinaryString(value);
+		while(str.length() < 32) {
+			str = "0" + str;
+		}
+		return str;
+	}
+	
+}
