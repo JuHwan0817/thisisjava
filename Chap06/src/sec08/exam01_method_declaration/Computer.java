0094d7056d36fab808a9f12dfcd07e863bc47a06
JuHwan <juhwan203@kakao.com>
2022-07-08 10:37:18 +0900

    CalculatorExample

--- Chap06/src/sec08/exam01_method_declaration/Computer.java
+++ Chap06/src/sec08/exam01_method_declaration/Computer.java
@@ -8,4 +8,12 @@ public class Computer {
 		}
 		return sum;
 	}
+	
+	int sum2(int ... values) { // ...(말 줄임표)를 써서 배열을 생략한다.
+		int sum = 0;
+		for(int i=0; i<values.length; i++) {
+			sum += values[i];
+		}
+		return sum;
+	}
 }
