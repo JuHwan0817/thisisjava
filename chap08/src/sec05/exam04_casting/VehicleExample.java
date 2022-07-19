845f26b7c66fddae9ee24df9e504a6f9275e8f97
JuHwan <juhwan203@kakao.com>
2022-07-19 11:44:20 +0900

    Example

--- chap08/src/sec05/exam04_casting/VehicleExample.java
+++ chap08/src/sec05/exam04_casting/VehicleExample.java
@@ -5,7 +5,14 @@ public class VehicleExample {
 	public static void main(String[] args) {
 		Vehicle vehicle = new Bus();
 		vehicle.run();
-		//vehicle.checkFare();
+		//vehicle.checkFare(); 형변환을 하지 않아서 실행 안됌 강제 형변환 시키면 가능
+		//Bus bus = (Bus) vehicle; 강제 타입 변환 하는법(vehicle)
+		//Bus bus = new Bus(); 자식에서 자식 개체라 실행 가능
+		//bus.run();
+		//bus.checkFare();
+		Bus bus = (Bus) vehicle; 
+		bus.run();
+		//bus.checkFare();
 	}
 
 }
---
+++ chap08/src/sec06/exam01_interface_extends/ImplementationC.java
@@ -0,0 +1,20 @@
+package sec06.exam01_interface_extends;
+
+public class ImplementationC implements InterfaceC {
+
+	@Override
+	public void methodA() {
+		System.out.println("ImplementationC-methodA() 실행");
+	}
+
+	@Override
+	public void methodB() {
+		System.out.println("ImplementationC-methodB() 실행");
+	}
+
+	@Override
+	public void methodC() {
+		System.out.println("ImplementationC-methodC() 실행");
+	}
+
+}
