0b12a5e9ba6b2d28b235ff3df30edc2d6041f930
JuHwan <juhwan203@kakao.com>
2022-08-23 16:43:20 +0900

    DaemonExample

---
+++ chap12/src/sec06/exam05_wait_notify/DataBox.java
@@ -0,0 +1,38 @@
+package sec06.exam05_wait_notify;
+
+public class DataBox {
+	private String data;
+
+	public synchronized String getData() {
+		if(this.data == null) {
+			try {
+				wait();
+			} catch (InterruptedException e) {}
+		}
+		String returnValue = data;
+		System.out.println("Consummerthread가 읽은 데이터: " + returnValue);
+		data = null;
+		notify();
+		return returnValue;
+	}
+
+	public synchronized void setData(String data) {
+		if(this.data != null) {
+			try {
+				wait();
+			} catch (InterruptedException e) {}
+		}
+		this.data = data;
+		System.out.println("PriducerThread가 생성한 데이터: " + data);
+		notify();
+	}
+	
+	
+}
+
+
+
+
+
+
+
